5ef12c065f0d02bd828145d4165e1ce1a4e18a44
PalakCodes27 <Palaksharmaa2004@gmail.com>
2025-12-12 17:59:25 +0530

    Template Litral problems

---
+++ .devcontainer/Es6_Topics/01_TemplateLiterals/index.js
@@ -0,0 +1,34 @@
+// First Problem
+const Name = "Mohit Garg";
+// console.log(`Hello, ${Name}! Welcome to JavaScript.`);
+
+// Second problem
+const num1 = 22;
+const num2 = 22;
+const output = num1 + num2;
+// console.log("Total count is", output);
+
+//Third Problem
+const user = {
+  userName: "mht",
+  city: "Blr",
+  country: "India",
+};
+console.log(`Name: ${user.userName} 
+Location: ${user.city} ${user.country}`);
+
+//Fourth Problem
+const product = {
+  title: "Laptop",
+  price: 45000,
+  rating: 4.5,
+};
+
+console.log(`Product Name: ${product.title}
+Product Price: ${product.price}
+Product Rating: ${product.rating}`);
+
+//Fifth Problem
+const numberInput = 5;
+console.log(`The square is ${numberInput * numberInput} 
+The Cube is ${numberInput * numberInput * numberInput}`);
