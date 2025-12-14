a65f7b00f7b281a203b00884a03430f7dfc01b27
PalakCodes27 <Palaksharmaa2004@gmail.com>
2025-12-14 10:58:23 +0530

    Push the template litral Folder 2

---
+++ .devcontainer/Es6_Topics/02_TemplateLiterals/index.js
@@ -0,0 +1,16 @@
+function buildURL(baseURL, endpoint, params) {
+    const queryString = Object.entries(params).map(([key, value]) => `${key}=${value}`).join("&");
+    return `${baseURL}/${endpoint}?${queryString}`;
+}
+
+const baseURL = "https://api.example.com";
+const endpoint = "products";
+const params = { page: 2, limit: 10, search: "laptop" };
+
+// console.log(`${baseURL}/${endpoint}${params}`)
+console.log("This is the complete URL:-",buildURL(baseURL, endpoint, params));
+
+
+function buildURL(para1, para2, para3) {
+  const mergeThirdPara = Object.entries(para3)
+}
